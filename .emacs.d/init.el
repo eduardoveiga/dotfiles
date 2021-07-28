30c5382074d60b10be4b39dbecaf11c7acb38e81
Eduardo Kluwe Veiga <eduardoveiga@protonmail.ch>
2021-07-28 12:17:01 -0300

    qua 28 jul 2021 12:17:01 -03

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -21,8 +21,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (pdf-tools password-mode magit rings company-tabnine company nginx-mode zenburn-theme sane-term markdown-mode go-mode emms twittering-mode telega ##))))
+   '(elcord gist pdf-tools password-mode magit rings company-tabnine company nginx-mode zenburn-theme sane-term markdown-mode go-mode emms twittering-mode telega ##)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
